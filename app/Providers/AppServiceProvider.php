b2f36c8ac4fc67520856ea87ee6c46b88507686e
unknown <mohammad.ali.e@hotmail.com>
2020-07-02 22:38:19 +0430

    app service provider https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        
+         if (config('app.production')) {
+            $url->forceScheme('https');
+        }
     }
 }
